1ec98a9f4f3ef99ee1ecc44ef985f2dcf445065d
EdsonReza <46760543+EdsonReza@users.noreply.github.com>
2023-03-21 19:29:47 -0600

    Cambio menor para mejorar lectura

--- busqueda/main.py
+++ busqueda/main.py
@@ -14,11 +14,14 @@ def main():
     # Crear el problema, con el estado inicial y el final, y las capacidades de las jarras
     jarra = Jarra(inicial, final, 1, 10)
 
+
+    print('BFS: \n')
     bfs = BreadthFirstSearch(jarra)
     solution_bfs = bfs.run()
     print(str(solution_bfs) + '\n')
     print('Nodos revisados: ' +  str(jarra.number))
 
+    print('\n\nDFS: \n')
     # La jarra cuenta los nodos, al reutilizar el mismo objeto se debe reiniciar
     jarra.number = 0
     dfs = DepthFirstSearch(jarra)
@@ -26,12 +29,14 @@ def main():
     print(str(solution_dfs) + '\n')
     print('Nodos revisados: ' +  str(jarra.number))
 
+    print('\n\nDFSL: \n')
     jarra.number = 0
     dfsl = DepthFirstSearchLimited(jarra)
     solution_dfsl = dfsl.run(5)
     print(str(solution_dfsl) + '\n')
     print('Nodos revisados: ' +  str(jarra.number))
 
+    print('\n\nDFSI: \n')
     jarra.number = 0
     dfsi = DepthFirstSearchIterative(jarra)
     solution_dfsi = dfsi.run(2)
